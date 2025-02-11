f1f887e0b409f775955b7f899cc48f5a12c6f439
Sahil Gupta <guptas3067@gmail.com>
2025-02-11 14:17:51 +0530

    hardcoded envs

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -6,8 +6,8 @@ export async function POST(req: NextRequest ) {
     console.log('contactData',contactData);
     
     try {
-      const supabaseUrl = process.env.SUPABASE_URL;
-      const supabaseKey = process.env.SUPABASE_ANON_KEY;
+      const supabaseUrl = 'https://xkoeunwvfcbamudnjzmc.supabase.co';
+      const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhrb2V1bnd2ZmNiYW11ZG5qem1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2ODc0NjcsImV4cCI6MjA1NDI2MzQ2N30.99KvMEmWG6aIqAFukWDUCd9tl3JFwuZAwE-n2eT3nJ8';
 
       if (!supabaseUrl || !supabaseKey) {
         return NextResponse.json({ success: false, message: 'Invalid Keys' });
